13eec809deacfb83dfd3b08b66b82d54e3039a22
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-10-07 10:55:35 -0700

    Initial Commit

---
+++ Pranksters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 07/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PrankstersTests/PrankstersTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrankstersTests.swift
+//  PrankstersTests
+//
+//  Created by Arpit iOS Dev. on 07/10/24.
+//
+
+import Testing
+@testable import Pranksters
+
+struct PrankstersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
