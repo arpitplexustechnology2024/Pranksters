4aeb95ec8a7915cea7ad31aa245fabbc301b913d
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-10-07 18:19:06 -0700

    Commit

---
+++ Pranksters/View/Cell/sideMenuCell.swift
@@ -0,0 +1,23 @@
+//
+//  sideMenuCell.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 07/10/24.
+//
+
+import UIKit
+
+class sideMenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
