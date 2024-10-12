883b24da4e13f45cfcddccd7433b833836b9a54c
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-10-11 18:05:44 -0700

    Commit

---
+++ Pranksters/View/Cell/RealisticCoverAllCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RealisticCoverAllCollectionViewCell.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 11/10/24.
+//
+
+import UIKit
+
+class RealisticCoverAllCollectionViewCell: UICollectionViewCell {
+    
+}
