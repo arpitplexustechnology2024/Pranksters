d10bd4562e1e127b85a93a74f9864cbd08faf44d
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-10-14 18:29:04 -0700

    Commit

---
+++ Pranksters/API Manager/FavouriteAPI.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteAPI.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 14/10/24.
+//
+
+import Foundation
---
+++ Pranksters/ViewModel/FavouriteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteViewModel.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 14/10/24.
+//
+
+import Foundation
