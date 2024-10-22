033b6cfc11acfc39e1d50a0fdadb99a60e074707
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-10-21 18:26:10 -0700

    Commit

---
+++ Pranksters/View/Cell/VideoCharacterAllCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  VideoCharacterAllCollectionViewCell.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 21/10/24.
+//
+
+import Foundation
---
+++ Pranksters/View/Cell/VideoCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  VideoCollectionViewCell.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 21/10/24.
+//
+
+import Foundation
