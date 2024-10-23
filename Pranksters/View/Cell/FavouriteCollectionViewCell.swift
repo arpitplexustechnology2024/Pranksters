3ef208b302c01479fb5ff91d8a864bdf2e0266a2
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-10-22 18:02:26 -0700

    Commit

---
+++ Pranksters/View/Cell/FavouriteCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FavouriteCollectionViewCell.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 22/10/24.
+//
+
+import UIKit
+
+class FavouriteCollectionViewCell: UICollectionViewCell {
+    
+}
