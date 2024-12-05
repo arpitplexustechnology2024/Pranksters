9f8671b3fea0419d27d3854148bc64e401947cec
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-12-05 09:43:14 -0800

    Commit

--- Prankster/Delegate/AppDelegate.swift
+++ Prankster/Delegate/AppDelegate.swift
@@ -75,7 +75,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -91,6 +90,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         checkNotificationAuthorization()
     }
-
 }
 
