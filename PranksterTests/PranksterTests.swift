e0b6f3491f8618bfb6c1028f864b7a663c905cad
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-12-04 19:28:20 -0800

    Initial Commit

---
+++ PranksterTests/PranksterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PranksterTests.swift
+//  PranksterTests
+//
+//  Created by Arpit iOS Dev. on 04/12/24.
+//
+
+import Testing
+@testable import Prankster
+
+struct PranksterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
